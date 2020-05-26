801d10188e2eedba9f63bf78a2a4fa94b3539916
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2020-05-26 10:16:01 +0800

    Update Api.php

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -4,6 +4,15 @@ namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
 
+//跨域設置
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    die();
+}
+
 class Api extends ResourceController
 {
     protected $format       = 'json';
